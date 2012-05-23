d80233caf09a5a8cdc56177b92baa9bcefd914bb
Patrick Peak <peakpg@gmail.com>
2012-05-23 13:47:16 -0400

    Isolate namespace
    * Need to isolate so that migrations can be installed.

--- lib/bcms_settings/engine.rb
+++ lib/bcms_settings/engine.rb
@@ -4,8 +4,7 @@ module BcmsSettings
   class Engine < Rails::Engine
     include Cms::Module
     
-    # Not isolating, since we have no routes and only one table, which will probably get inlined eventually.
-#    isolate_namespace BcmsSettings
+    isolate_namespace BcmsSettings
 
     initializer 'bcms_settings', :after=>'disable_dependency_loading' do
       require 'bcms_settings/cms/settings'
--- lib/bcms_settings/routes.rb
+++
@@ -1,9 +0,0 @@
-module Cms::Routes
-  def routes_for_bcms_settings
-    namespace(:cms) do
-      # No routes for this module.
-    end
-  end
-end
-
-
